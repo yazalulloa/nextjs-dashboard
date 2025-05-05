1d8bdc417339ac8ad8527f0b5c7759119a375561
Yazal Ulloa <yazal.ulloa@asesormasmovil.es>
2025-05-05 19:51:11 -0400

    chapter 16

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,6 +1,11 @@
 import SideNav from '@/app/ui/dashboard/sidenav';
+import {Metadata} from "next";
 
 export const experimental_ppr = true;
+
+export const metadata: Metadata = {
+  title: 'Dashboard',
+};
 
 export default function Layout({children}: { children: React.ReactNode }) {
   return (
